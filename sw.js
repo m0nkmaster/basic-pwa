94819728124e6f7236d396da7739e5c7ed75f064
Rob MacDonald <rob@robmacdonald.com>
2016-09-27 12:23:53 +0100

    Updated SW

--- sw.js
+++ sw.js
@@ -27,7 +27,23 @@ self.addEventListener('fetch', function(e) {
   } else {
     console.log = 'Not a JSON request, just let through';
     e.respondWith(
-      response
+        caches.match(event.request).then(function(response) {
+        if (response) {
+          console.log('Found ' + e.request.url + ' response in cache:', response);
+          return response;
+        }
+        console.log('No response found in cache. About to fetch from network...');
+
+        return fetch(event.request).then(function(response) {
+          console.log('Response from network is:', response);
+
+          return response;
+        }).catch(function(error) {
+          console.error('Fetching failed:', error);
+
+          throw error;
+        });
+      })
     )
   }
  });
