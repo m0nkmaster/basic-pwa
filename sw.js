1549f9d48d6668b17a2532171cc72adcd7bba6be
Rob MacDonald <rob@robmacdonald.com>
2016-09-27 11:01:01 +0100

    JSON stories added

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
+var CHANGE_NAME = 'm0nkmaster';
+var urlsToCache = [
+  '',
+  'data/stories.json'
+];
+
 self.addEventListener('install', function(event) {
   // Perform install steps
 
   console.log("Hi I'm the service worker you've heard so much about");
+
+  console.log('Doing cache setup');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
