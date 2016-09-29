980fa9b5bbc2910ee617daf89df9570f12ec576e
Josh Tumath <josh.tumath@bbc.co.uk>
2016-09-29 13:41:46 +0100

    Caching data

--- sw.js
+++ sw.js
@@ -1,5 +1,42 @@
 'use strict';
 
+const CACHE_VERSION = 1;
+
+const CURRENT_CACHE = {
+  data: 'data-cache-v' + CACHE_VERSION,
+  templates: 'templates-cache-v' + CACHE_VERSION
+};
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CURRENT_CACHE.data)
+    .then(cache => {
+      return cache.add('data/stories.json')
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+    return CURRENT_CACHES[key];
+  });
+
+  // Active worker won't be treated as activated until promise resolves successfully.
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (expectedCacheNames.indexOf(cacheName) == -1) {
+            console.log('Deleting out of date cache:', cacheName);
+            
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   // We only want to call event.respondWith() if this is a navigation request
   // for an HTML page.
@@ -10,8 +47,10 @@ self.addEventListener('fetch', event => {
       (event.request.method === 'GET' &&
        event.request.headers.get('accept').includes('text/html'))) {
     console.log('Handling fetch event for', event.request.url);
+
     event.respondWith(
-      fetch(event.request).catch(error => {
+      fetch(event.request)
+      .catch(error => {
         // The catch is only triggered if fetch() throws an exception, which will most likely
         // happen due to the server being unreachable.
         // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx
